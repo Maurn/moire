5af59a84f604e4b0731bf28bfba7ec08c4f3f16f
Maurn <maurn95@gmail.com>
2017-02-18 00:55:41 +0100

    Move all unnecessary initialization logic out of draw(). Still slow..

--- src/Sketch.java
+++ src/Sketch.java
@@ -9,6 +9,9 @@ import java.util.ArrayList;
 public class Sketch extends PApplet {
     private ArrayList<Disc> discs;
 
+    private PVector center;
+    private float interval;
+
     public void settings() {
         fullScreen(2);
     }
@@ -22,26 +25,27 @@ public class Sketch extends PApplet {
         for (int i = 0; i < 8; i++) {
             discs.add(new Disc(this, new PVector(width / 2, height / 2), 500, 20));
         }
+
+        center = new PVector(width / 2, height / 2);
+        interval = (2 * PI) / discs.size();
     }
 
     public void draw() {
         background(0);
 
         float r = 0;
-        float interval = (2 * PI) / discs.size();
-        PVector center = new PVector(width / 2, height / 2);
+
         PVector target = new PVector(-1, -1);
-        target.setMag(PVector.sub(new PVector(mouseX, mouseY), center).mag()*0.3f);
-        target.rotate(atan2(mouseY-height/2, mouseX-width/2));
+        target.setMag(PVector.sub(new PVector(mouseX, mouseY), center).mag() * 0.3f);
+        target.rotate(atan2(mouseY - height / 2, mouseX - width / 2));
         for (Disc disc : discs) {
             target.rotate(r);
-            PVector loc = PVector.add(center, target);
-            disc.setLocation(loc);
+            disc.setLocation(PVector.add(center, target));
             r += interval;
 
             disc.display();
         }
 
-        text(frameRate, 50,50);
+        text(frameRate, 50, 50);
     }
 }
